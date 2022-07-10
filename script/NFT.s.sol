871c39f99868d2338a418dec1fc4946c8eae9c2d
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-07-10 09:51:34 +0900

    Add contract and its test

--- script/NFT.s.sol
+++ script/NFT.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/NFT.sol";
 
 contract NFTScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+        NFT nftContract = new NFT("MyNFT", "NFT", "https://example.com/");
+
+        vm.stopBroadcast();
     }
 }
