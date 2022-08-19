853241f81e4d4ed54d74d6a8473c2736a4a932e1
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2022-08-19 22:08:16 +0900

    increase deploy fee

--- test/FizzBuzz.t.sol
+++ test/FizzBuzz.t.sol
@@ -5,31 +5,80 @@ import "forge-std/Test.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract FizzBuzz {
-    mapping(uint256 => address) public poohStore;
+    struct Pooh {
+        string text;
+    }
+    mapping(string => Pooh) public poohStore;
     string[] public fizzbuzzList;
 
     function run(uint256 i) public returns (string memory) {
+        require(i >= 0, "i is less than 0");
+
         if (i % 15 == 0) {
-            poohStore[i] = msg.sender;
-            fizzbuzzList.push("FizzBuzz");
+            calculateText(i, safeFizzBuzzText("FizzBuzz"));
         } else if (i % 3 == 0) {
-            poohStore[i] = msg.sender;
-            fizzbuzzList.push("Fizz");
+            calculateText(i, safeFizzText("Fizz"));
         } else if (i % 5 == 0) {
-            poohStore[i] = msg.sender;
-            fizzbuzzList.push("Buzz");
+            calculateText(i, safeBuzzText("Buzz"));
         } else {
-            poohStore[i] = msg.sender;
-            fizzbuzzList.push(Strings.toString(i));
+            calculateText(i, Strings.toString(i));
         }
 
+        string memory result = createResponse();
+
+        delete fizzbuzzList;
+        return string(result);
+    }
+
+    function getPoohText(uint256 i) public view returns (string memory) {
+        return poohStore[Strings.toString(i)].text;
+    }
+
+    function calculateText(uint256 i, string memory t)
+        public
+        returns (string memory)
+    {
+        Pooh memory pooh;
+        pooh.text = t;
+        string memory idx = Strings.toString(i);
+        poohStore[idx] = pooh;
+        fizzbuzzList.push(poohStore[idx].text);
+        return "";
+    }
+
+    function createResponse() public view returns (string memory) {
         string memory result = "";
         for (uint256 j = 0; j < fizzbuzzList.length; j++) {
             result = string.concat(result, string.concat(fizzbuzzList[j]));
         }
+        return result;
+    }
 
-        delete fizzbuzzList;
-        return string(result);
+    function safeFizzBuzzText(string memory t)
+        public
+        pure
+        returns (string memory)
+    {
+        require(isSame(t, "FizzBuzz"), "t is not FizzBuzz");
+        return t;
+    }
+
+    function safeFizzText(string memory t) public pure returns (string memory) {
+        require(isSame(t, "Fizz"), "t is not Fizz");
+        return t;
+    }
+
+    function safeBuzzText(string memory t) public pure returns (string memory) {
+        require(isSame(t, "Buzz"), "t is not Buzz");
+        return t;
+    }
+
+    function isSame(string memory a, string memory b)
+        public
+        pure
+        returns (bool)
+    {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
     }
 }
 
